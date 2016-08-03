fdc93557e097119ce0d36187196d69ed48f9fcdf
Volodymyr Kuliavets <volodymyr.kuliavets@gmail.com>
2016-08-04 00:21:58 +0300

    Corrected form and make email as the username

--- itaplay/itaplay/authentication/forms.py
+++ itaplay/itaplay/authentication/forms.py
@@ -3,13 +3,15 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
+    username = forms.EmailField(label="User name (e-mail):")
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('first_name', 'last_name', 'username', 'password')
 
 class AdviserUserForm(forms.ModelForm):
+    avatar = forms.URLField(label="Avatar (URL):")
     class Meta:
         model = AdviserUser
         fields = ('avatar',)
--- itaplay/itaplay/authentication/views.py
+++ itaplay/itaplay/authentication/views.py
@@ -9,8 +9,10 @@ def register(request):
         baseForm = UserForm(request.POST)
         extendedForm = AdviserUserForm(request.POST)
         if baseForm.is_valid() and extendedForm.is_valid():
+            newBaseUser = baseForm.save(commit=False)
+            newBaseUser.email = baseForm.data[u'username']
+            newBaseUser.save()
 
-            newBaseUser = baseForm.save()
             newExtendedUser = extendedForm.save(commit=False)
             newExtendedUser.user = newBaseUser
             newExtendedUser.save()
