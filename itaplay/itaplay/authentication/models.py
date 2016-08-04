eb91f80d085995961806143ac78835295d51e386
Yuriy Voznyak <voznjak@yahoo.com>
2016-08-04 11:38:44 +0300

    added invitation model

--- itaplay/itaplay/authentication/models.py
+++ itaplay/itaplay/authentication/models.py
@@ -7,3 +7,8 @@ class AdviserUser(models.Model): # name of our project is Adviser
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.URLField()
 
+class AdviserInvitations(models.Model):
+    email=models.EmailField()
+    ID_company=models.IntegerField()
+    verification_code=models.CharField(max_length=128)
+    is_active=models.BooleanField()
