dde817236542f54107d72c59c372ff8d234da9b4
Sakivskyy Igor <Sakivskyy.igor@gmail.com>
2016-08-04 13:14:01 +0300

    create general layout with angular route basic example +  ng-include

--- itaplay/itaplay/static/js/app/app.js
+++ itaplay/itaplay/static/js/app/app.js
@@ -1,16 +1,28 @@
 'use strict';
 
-var itaplay = angular.module('itaplay', ['ngRoute',]);
+var itaplay = angular.module('itaplay', ['ngRoute', 'ngMaterial']);
 
 
 itaplay.config(function($routeProvider) {
-    $routeProvider.
-        when('/test', {
+    $routeProvider
+    	.when('/test', {
             templateUrl: '../../../static/js/app/test/views/test.html',
             controller: TestController
-        }).
-        otherwise({redirectTo: '/test'});
-}).
-run(function($log) {
+        })
+        .otherwise({redirectTo: '/test'})
+
+        .when('/test1', {
+            templateUrl: '../../../static/js/app/test/views/test1.html',
+            
+        })
+        .otherwise({redirectTo: '/test'})
+
+        .when('/clips', {
+            templateUrl: '../../../static/js/app/test/views/clips.html',
+            
+        })
+        .otherwise({redirectTo: '/test'});
+})
+.run(function($log) {
     $log.info("Starting up");
 });
