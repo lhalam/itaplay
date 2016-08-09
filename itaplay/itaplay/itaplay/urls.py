7f3c2815f52c9f7930f7c2b6e4acc8af7c5aeabd
bohdankorchak <bohdankorchak@gmail.com>
2016-08-09 19:46:07 +0300

    Add static files

--- itaplay/itaplay/itaplay/urls.py
+++ itaplay/itaplay/itaplay/urls.py
@@ -19,5 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('home.urls')),
     url(r'^admin/', admin.site.urls),
-
 ]
--- itaplay/itaplay/static/js/app/app.js
+++ itaplay/itaplay/static/js/app/app.js
@@ -1,16 +1,47 @@
 'use strict';
 
-var itaplay = angular.module('itaplay', ['ngRoute',]);
+var itaplay = angular.module('itaplay', ['ngRoute','ngMaterial']);
 
 
 itaplay.config(function($routeProvider) {
-    $routeProvider.
-        when('/test', {
+    $routeProvider
+        .when('/test/', {
             templateUrl: '../../../static/js/app/test/views/test.html',
             controller: TestController
-        }).
-        otherwise({redirectTo: '/test'});
-}).
-run(function($log) {
+        })
+        .otherwise({redirectTo: '/test'})
+
+        .when('/company/', {
+            templateUrl: '../../../static/js/app/company/views/all_companies.html',
+            controller: AllCompanyController
+            
+           
+        })
+       .otherwise({redirectTo: '/test'})
+       
+       .when('/company/add_new/', {
+            templateUrl: '../../../static/js/app/company/views/add_company.html',
+            controller: CompanyAddController
+                        
+           
+        })
+        .otherwise({redirectTo: '/company/'});
+})
+
+.run(function($log) {
     $log.info("Starting up");
+})
+// choose colors for our theme
+.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('teal')
+      .accentPalette('blue')
+      .dark();
+
+
+})
+
+.config(function($httpProvider){
+$httpProvider.defaults.xsrfCookieName = 'csrftoken';
+$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 });
