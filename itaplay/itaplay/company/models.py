57cf126926d23865003bc18b6a9c3cba32ee0322
bohdankorchak <bohdankorchak@gmail.com>
2016-08-11 09:26:27 +0300

    Add fields at company

--- itaplay/itaplay/company/models.py
+++ itaplay/itaplay/company/models.py
@@ -5,10 +5,11 @@ from django.db import models
 
 class Company(models.Model):
     """account for company"""
-    company_logo = models.URLField( unique=True)
+    company_zipcode = models.CharField(max_length=20, default="")
+    company_logo = models.URLField(default="")
     company_name = models.CharField(max_length=200, unique=True)
     company_mail = models.EmailField(unique=True)
     company_phone = models.CharField(max_length=50, unique=True)
-    company_address = models.CharField(max_length=200, unique=True)
+    company_address = models.CharField(max_length=200)
     administrator = models.IntegerField(default=1) # will be foreign key
     
