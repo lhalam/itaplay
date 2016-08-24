0efd8ac319c8891523866502765b21a449415d04
George Curious <curious725@gmail.com>
2016-08-24 15:30:01 +0300

    fix formatting

--- itaplay/itaplay/static/js/app/templates/controllers/current-template-controller.js
+++ itaplay/itaplay/static/js/app/templates/controllers/current-template-controller.js
@@ -1,22 +1,21 @@
 function CurrentTemplateController($scope,
     $http, $routeParams, $location) {
-
-    $scope.init = function(){
-    $scope.pk = $routeParams.pk;
-    $http({
-        method : "GET",
-        url : '/templates/current/' + $scope.pk
-    }).then(function mySuccess(response) {
-        $scope.data = response.data;
-    }, function myError(response) {
-        $scope.error_msg = response.statusText;
-    });
-};
-
-    $scope.deleteCurrent = function () {
+    $scope.init = function() {
+        $scope.pk = $routeParams.pk;
         $http({
-            method : "DELETE",
-            url : '/templates/delete/' + $scope.pk
+            method: "GET",
+            url: '/templates/current/' + $scope.pk
+        }).then(function mySuccess(response) {
+            $scope.data = response.data;
+        }, function myError(response) {
+            $scope.error_msg = response.statusText;
+        });
+    };
+
+    $scope.deleteCurrent = function() {
+        $http({
+            method: "DELETE",
+            url: '/templates/delete/' + $scope.pk
         }).then(function mySuccess(response) {
             $location.path('/#/templates');
         });
