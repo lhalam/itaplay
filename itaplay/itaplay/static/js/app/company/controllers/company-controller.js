45277927751d1ef3a36cae0c1ab133ae2a6d7ec2
bohdankorchak <bohdankorchak@gmail.com>
2016-08-25 09:42:29 +0300

    Put delete and update functions out of init

--- itaplay/itaplay/static/js/app/company/controllers/company-controller.js
+++ itaplay/itaplay/static/js/app/company/controllers/company-controller.js
@@ -1,30 +1,26 @@
 
 function AllCompanyController($scope, $http, $location) {
-
   $scope.init = function(){
-
     $http.get("company/company_view/").then(function (response) {
       $scope.companies = response.data;
      }, function(response) {
           console.log(response);
         $scope.data = "Something went wrong";
     });
-    $scope.delete = function(company){
-        $http.delete("company/delete_company/"+company.pk, {"company_id": company.pk}).then(function (response) {
-         $scope.companies = response.data;
-        });
-     };      
   };
+  $scope.delete = function(company){
+    $http.delete("company/delete_company/"+company.pk, {"company_id": company.pk}).then(function (response) {
+    $scope.companies = response.data;
+    });
+  };      
 };
 
 function CompanyAddController($scope, $http,  $location) {
-  $scope.init = function(){
-    $scope.save = function (company){
-      $http.post("company/company_view/", company).success(function (company) {
-        $location.path('/company');
-      });
-    };        
-  };
+  $scope.save = function (company){
+    $http.post("company/company_view/", company).success(function (company) {
+      $location.path('/company');
+    });
+  };        
 };
 
 function CompanyController($scope, $http, $routeParams, $location) {
@@ -36,16 +32,16 @@ function CompanyController($scope, $http, $routeParams, $location) {
           console.log(response);
           $scope.data = "Something went wrong";
       });
-      $scope.deleteCurrent = function(company){
-        $http.delete("company/delete_company/"+id, {"company_id":id}).then(function (company) {
-          $location.path('/company');
-        });
-      };      
-      $scope.update = function(company){
-        $http.post("company/company_view/", company).success(function (company) {
-          $location.path('/company');
-        });
-      };    
-  };
+    };
+    $scope.deleteCurrent = function(company){
+      $http.delete("company/delete_company/"+id, {"company_id":id}).then(function (company) {
+        $location.path('/company');
+      });
+    };      
+    $scope.update = function(company){
+      $http.put("company/company_view/", company).success(function (company) {
+        $location.path('/company');
+      });
+    };     
 };
 
