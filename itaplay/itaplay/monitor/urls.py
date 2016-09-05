407a6e68a9db316964fcd87a979bb5e2d83547ce
bohdankorchak <bohdankorchak@gmail.com>
2016-09-05 18:05:49 +0300

    Add monitor view

--- itaplay/itaplay/monitor/urls.py
+++ itaplay/itaplay/monitor/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.MonitorView.as_view(), name='monitor'),
-
+    url(r'^$', views.GetMonitorView.as_view(), name='monitor'),
+   # url(r'^get_by_mac/(?P<mac>)/$', views.MonitorView.as_view(), name='monitor'),
 ]
--- itaplay/itaplay/monitor/views.py
+++ itaplay/itaplay/monitor/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render, render_to_response
 from django.views.generic.base import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.forms.models import model_to_dict
+from player.models import Player
+from projects.models import AdviserProject
 
-
-class MonitorView(View):
+class GetMonitorView(View):
     def get(self, request):
         return render_to_response('monitor.html')
+
+class MonitorView(View):    
+    def get(self, request, mac):
+        player = Player.objects.get(mac_address=mac)
+        project = player.project
+        data = model_to_dict(project)
+        return HttpResponse({"data" : data})
+   
