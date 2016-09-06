5bd1fc8fb7370a68657cc19bf95ce21d79b65d0d
Taras Stankovskyi <stankovskytaras@gmail.com>
2016-09-06 11:28:44 +0300

    fix

--- itaplay/itaplay/static/js/app/monitor/controllers/monitor-controller.js
+++ itaplay/itaplay/static/js/app/monitor/controllers/monitor-controller.js
@@ -2,7 +2,7 @@ function MonitorController($scope, $http, $routeParams) {
     var mac = $routeParams.mac;
     $scope.init = function( ) {
     $http.get('get_by_mac/'+ mac).then(function(response){
-        $scope.project = response.data;
+        $scope.template = response.data.template;
     }, function(response) {
           console.log(response);
           $scope.data = "Something went wrong";});
