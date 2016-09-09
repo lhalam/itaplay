2e78e67ca98b4b4fb5bedb8c2717d1a3c1c5b00a
Taras Stankovskyi <stankovskytaras@gmail.com>
2016-09-09 12:35:46 +0300

    monitor shows all type of  media

--- itaplay/itaplay/static/js/app/monitor/controllers/monitor-controller.js
+++ itaplay/itaplay/static/js/app/monitor/controllers/monitor-controller.js
@@ -1,20 +1,18 @@
 function MonitorController($scope, $http, $routeParams) {
     var mac = $routeParams.mac;
     $scope.init = function( ) {
-    $http.get('get_by_mac/'+ mac).then(function(response){
-        $scope.template = response.data.template;
-
-
+        $http.get('get_by_mac/'+ mac).then(function(response) {
             if (window.DOMParser) {
                 parser = new DOMParser();
                 xmlDoc = parser.parseFromString(response.data.template, "text/xml");
-            }
-            else // Internet Explorer
-            {
+                }
+            else { // Internet Explorer
+
                 xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
                 xmlDoc.async = false;
                 xmlDoc.loadXML(response.data.template);
-            }
+                }
+
             $scope.areas = [];
             DOM_areas = xmlDoc.getElementsByTagName("area");
             for (var i = 0; i < DOM_areas.length; i++) {
@@ -31,19 +29,23 @@ function MonitorController($scope, $http, $routeParams) {
                     $scope.areas[i]['clips'][k] = {};
                     $scope.areas[i]['clips'][k]['src'] = DOM_clip[k].attributes.src.nodeValue;
                 };
-
-
-                };alert($scope.areas[0]['id'])
-
-
-
-
-
-
-
-
-    }, function(response) {
-          console.log(response);
-          $scope.data = "Something went wrong";});
+            };
+        }, function(response) {
+            console.log(response);
+            $scope.data = "Something went wrong";
+        });
     };
+
+    $scope.checkImage = function(value) {
+        console.log("checkImage", value);
+        var format = value.split('.').pop();
+        return ['jpg', 'img', 'png', 'gif'].includes(format) ? true : false
+
     };
+
+    $scope.checkVideo = function(value) {
+        var format = value.split('.').pop();
+        return ['mp4', 'avi', 'asf', 'flv'].includes(format) ? true : false
+    };
+
+};
