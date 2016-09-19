d52e4dc12a32c96cdd9a0ddf4a4f1ae38837169c
bohdankorchak <bohdankorchak@gmail.com>
2016-09-20 00:49:34 +0300

    Restrict getting company by logined user

--- itaplay/itaplay/static/js/app/company/controllers/company-controller.js
+++ itaplay/itaplay/static/js/app/company/controllers/company-controller.js
@@ -3,7 +3,7 @@ function AllCompanyController($scope, $http, $location) {
 
   $scope.init = function(){
 
-    $http.get("company/company_view/").then(function (response) {
+    $http.get("company/company_list_view/").then(function (response) {
       $scope.companies = response.data;
      }, function(response) {
           console.log(response);
@@ -11,15 +11,15 @@ function AllCompanyController($scope, $http, $location) {
     });
   };
   $scope.delete = function(company){
-    $http.delete("company/delete_company/"+company.pk, {"company_id": company.pk}).then(function (response) {
-      $scope.companies = response.data;
+    $http.delete("company/company_details_view/"+company.id, {"company_id": company.id}).then(function (response) {
+        $location.path('/company');
     });
   };      
 };
 
 function CompanyAddController($scope, $http,  $location) {
   $scope.save = function (company){
-    $http.post("company/company_view/", company).success(function (company) {
+    $http.post("company/company_list_view/", company).success(function (company) {
       $location.path('/company');
     });
   };        
@@ -28,21 +28,26 @@ function CompanyAddController($scope, $http,  $location) {
 function CompanyController($scope, $http, $routeParams, $location) {
   var id = $routeParams.company_id;
   $scope.init = function(){
-    $http.get("company/current_company_view/"+id).then(function (response) {
+    $http.get("company/company_details_view/"+id).then(function (response) {
       $scope.company = response.data.company;
       $scope.users = response.data.users;
     }, function(response) {
+          $location.path('/company');
           console.log(response);
           $scope.data = "Something went wrong";
       });
     };
     $scope.deleteCurrent = function(company){
-      $http.delete("company/delete_company/"+id, {"company_id":id}).then(function (company) {
+      $http.delete("company/company_details_view/"+id, {"company_id":id}).then(function (company) {
         $location.path('/company');
-      });
+      }, function(response) {
+          console.log(response);
+          $scope.data = response;
+          $location.path('/company');
+        });
     };      
     $scope.update = function(company){
-      $http.put("company/company_view/", company).success(function (company) {
+      $http.put("company/company_details_view/"+id, company).success(function (company) {
         $location.path('/company');
       });
     };    
