98b440a6bab28b8120f003322eec3f6b6e0a2610
Yuri H <halapup.yurii@gmail.com>
2016-09-23 23:19:51 +0300

    some tests for xml_templates views

--- itaplay/itaplay/tests/xml_templates/test_views.py
+++ itaplay/itaplay/tests/xml_templates/test_views.py
@@ -2,35 +2,35 @@ import json
 
 from django.test import TestCase, Client
 from xml_templates.models import XmlTemplate
+from django.contrib.auth.models import User
+
+from django.core.urlresolvers import reverse
 
 
 class XmlTemplateViewTests(TestCase):
 
     def setUp(self):
+
+        User.objects.create(
+            id=1,
+            username="superuser@user.com",
+            email="super@user.com",
+            password="password1",
+            is_superuser=True,
+        )
+
+        User.objects.create(
+            id=2,
+            username="user@user.com",
+            email="user@user.com",
+            password="password2",
+            is_superuser=False)
+
         self.client = Client()
 
-    def test_get_xml_templates_not_login(self):
-        response = self.client.get('/#/templates')
-        self.assertEqual(response.status_code, 302)
-
-    # def test_get_xml_template_if_login(self):
-    #     response = self.client.get('/#/templates')
-    #     self.
-
-    def test_login(self):
-        user_data = json.dumps(
-            {'username': 'test@test.com', 'password': 'password'})
-        response = self
-
-    # def test_xml_templates_list(self):
-    #     self.login()
-    #     self.url = "#/templates"
-    #     response = self.client.get(self.url)
-    #     self.assertEqual(response.status_code, 200)
-
-    # def login(self):
-    #     user_data = json.dumps(
-    #         {'username': "test@test.com", 'password': "rootroot"})
-
-    #     self.client.post("/auth/login", data=user_data,
-    #                      content_type='application/json', follow=True)
+        def test_get_list_xml_templates_loads():
+            self.client.login(username="superuser@user.com",
+                              password="password1")
+            url = reverse('templates_list')
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
