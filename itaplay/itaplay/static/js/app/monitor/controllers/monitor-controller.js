973308e919779b9efdc5f0f259ec484c9597855b
bohdankorchak <bohdankorchak@gmail.com>
2016-09-23 11:42:07 +0300

    fix slider for images

--- itaplay/itaplay/static/js/app/monitor/controllers/monitor-controller.js
+++ itaplay/itaplay/static/js/app/monitor/controllers/monitor-controller.js
@@ -50,15 +50,15 @@ function MonitorController($scope, $rootScope, $http, $routeParams,  $interval) 
 
     $scope.currentIndex=[];
 
-    $scope.isCurrentSlideIndex = function (id_area, index, clips) {
+    $scope.isCurrentSlideIndex = function (id_area, index) {
         return $scope.currentIndex[id_area] === index;
     };
 
     var ImageSlider = function(areas){
         areas.forEach(function(area) {
           $scope.currentIndex[area['id']] = 0;
-          clips = area['clips']
-          $interval(function(){$scope.currentIndex[area['id']] = ($scope.currentIndex[area['id']] < clips.length - 1) ? ++$scope.currentIndex[area['id']] : 0;}, 10000);
+          //clips = area['clips']
+          $interval(function(){$scope.currentIndex[area['id']] = ($scope.currentIndex[area['id']] < area['clips'].length - 1) ? ++$scope.currentIndex[area['id']] : 0;}, 5000+area['id']*400);
         });
     };
 };
