55402773cc3e3b146ea6d522d1b0926d0c96a64a
bohdankorchak <bohdankorchak@gmail.com>
2016-09-27 19:53:49 +0300

    Fix 'project to players view'

--- itaplay/itaplay/projects/views.py
+++ itaplay/itaplay/projects/views.py
@@ -99,7 +99,7 @@ class AdviserProjectToPlayers(View):
         :return: Http response with list of players that have current project
         """
         project = AdviserProject.objects.get(id=project_id)
-        if project.id_company.id != request.user.adviseruser.id_company.id:
+        if (not request.user.is_superuser) and (project.id_company.id != request.user.adviseruser.id_company.id):
             return HttpResponseBadRequest("Permission denied")
         players = Player.objects.filter(project=project_id)
         data = [model_to_dict(i) for i in players]
@@ -115,7 +115,7 @@ class AdviserProjectToPlayers(View):
         if (not data.get("players")):
             return HttpResponseBadRequest("Players are not added. Please, add some players.")
         project = AdviserProject.objects.get(id = data.get("project")["id"])
-        if project.id_company.id != request.user.adviseruser.id_company.id:
+        if (not request.user.is_superuser) and (project.id_company.id != request.user.adviseruser.id_company.id):
             return HttpResponseBadRequest("Permission denied")
         for obj in data.get("players"):
             player = Player.get_by_id(obj["id"])
