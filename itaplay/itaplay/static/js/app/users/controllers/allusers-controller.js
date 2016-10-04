a2120e743193c20efcc49f97ade11c0175633d65
Sakivskyy Igor <Sakivskyy.igor@gmail.com>
2016-10-04 21:30:24 +0300

    small fix

--- itaplay/itaplay/static/js/app/users/controllers/allusers-controller.js
+++ itaplay/itaplay/static/js/app/users/controllers/allusers-controller.js
@@ -1,16 +1,3 @@
-// function AllUsersController($scope, $http) {
-
-//   $scope.init = function(){
-//  };
-//     $http.get("/users/all/").then(function (response) {
-//       $scope.users = response.data});
-//     //  }, function(response) {
-//     //     $scope.data = "Something went wrong";
-//     // });
- 
-  
-// };
-
 function AllUsersController($scope, $http) {
     $scope.init = function() {
         var api_url = '/users/all/';
