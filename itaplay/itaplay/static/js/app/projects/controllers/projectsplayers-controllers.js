a717a41d5a2be4d337f7ca20ac07acc325fa6bff
bohdankorchak <bohdankorchak@gmail.com>
2016-10-07 22:53:29 +0300

    Fix design for companies, make front-end code more clean

--- itaplay/itaplay/static/js/app/projects/controllers/projectsplayers-controllers.js
+++ itaplay/itaplay/static/js/app/projects/controllers/projectsplayers-controllers.js
@@ -1,17 +1,18 @@
 function DialogController ($scope, $http, $location, $mdDialog, parent) {
-  $scope.parent = parent;
-  $scope.init = function(){
-    $http.get("player/player_view/").then(function (response) {
-      $scope.players = response.data;
-     }, function(response) {
-          console.log(response);
-        $scope.data = "Something went wrong";
-    });
-  };
-  $scope.cancel = function () {
-    $mdDialog.cancel();
-  };
-  $scope.answer = function (answer) {
-    $mdDialog.hide(answer);
-  };  
+    $scope.parent = parent;
+    $scope.init = function () {
+        $http.get("player/player_view/").then(function (response) {
+            $scope.players = response.data;
+        }, function (response) {
+            $scope.data = "Something went wrong";
+        });
+    };
+
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
+    
+    $scope.answer = function (answer) {
+        $mdDialog.hide(answer);
+    };  
 };
