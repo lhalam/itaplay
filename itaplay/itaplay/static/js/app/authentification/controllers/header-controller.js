c37a3e126a4680a1250cfa4565fdd18b1f98d3b9
Taras Stankovskyi <stankovskytaras@gmail.com>
2016-10-08 13:35:03 +0300

    refactor UI logout

--- itaplay/itaplay/static/js/app/authentification/controllers/header-controller.js
+++ itaplay/itaplay/static/js/app/authentification/controllers/header-controller.js
@@ -1,11 +1,20 @@
 angular.module('LogoutController',['ngMaterial'])
 .controller('LogoutController', function($scope,$http, $window) {
 
+    $scope.init = function() {
+    $http.get("/users/all/").then(function (response) {
+      $scope.avatar = response.data.AdviserUser.avatar;
+     }, function(response) {
+          console.log(response);
+        $scope.data = "Something went wrong";
+    });
+  };
+
     $scope.LogoutUser = function() {
         $http.get('/auth/logout').then(function(response) {
                 $window.location.href = '/';
             }, function errorCallback(response) {
             });
-    }
+  }
 });
 
