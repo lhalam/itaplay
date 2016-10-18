0f5df550fd612a41581734d088fcea4c3d5ac70f
bohdankorchak <bohdankorchak@gmail.com>
2016-10-18 10:33:34 +0300

    fix player design

--- itaplay/itaplay/static/js/app/player/controllers/player-controller.js
+++ itaplay/itaplay/static/js/app/player/controllers/player-controller.js
@@ -7,16 +7,15 @@ function AllPlayerController($scope, $http, $location) {
     $http.get("player/player_view/").then(function (response) {
       $scope.players = response.data;
      }, function(response) {
-          console.log(response);
         $scope.data = "Something went wrong";
     });
   };
 
       $scope.delete = function(player){
         $http.delete("player/delete_player/"+player.id, {"id": player.id}).then(function (player) {   
-          $location.path('/#/player');
+          $location.path('/player');
         },function (player) {
-            $location.path('/#/player');
+            $location.path('/player');
         }); 
      };
 
@@ -28,10 +27,10 @@ function AllPlayerController($scope, $http, $location) {
 };   
 
 function PlayerAddController($scope, $http,  $location) {
-  $scope.initadd = function(){
+  $scope.init = function(){
     $scope.save = function (player){
       $http.post("player/player_view/", player).success(function (player) {
-        $location.path('/#/player');
+        $location.path('/player');
       });
     };        
   };
@@ -43,19 +42,18 @@ function PlayerController($scope, $http, $routeParams,  $location) {
     $http.get("player/current_player_view/"+id).then(function (response) {
       $scope.player = response.data.player;
     }, function(response) {
-          console.log(response);
           $scope.data = "Something went wrong";
       });
       $scope.delete_current = function(player){
         $http.delete("player/delete_player/"+id, {"pk":id}).then(function (player) {
-          $location.path('/#/player');
+          $location.path('/player');
         }, function (player) {
-            $location.path('/#/player');
+            $location.path('/player');
         });
       };      
       $scope.update = function(player){
         $http.put("player/player_view/", player).success(function (player) {
-          $location.path('/#/player');
+          $location.path('/player');
         });
       };      
   };
