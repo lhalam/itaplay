f40dafbf4b26a4c88b96067234cbdb1fbda948cb
Taras Stankovskyi <stankovskytaras@gmail.com>
2016-10-19 14:28:36 +0300

    add username to header

--- itaplay/itaplay/static/js/app/authentification/controllers/header-controller.js
+++ itaplay/itaplay/static/js/app/authentification/controllers/header-controller.js
@@ -4,6 +4,8 @@ angular.module('LogoutController',['ngMaterial'])
     $scope.init = function() {
     $http.get("/users/profile/").then(function (response) {
       $scope.avatar = response.data.AdviserUser.avatar;
+      $scope.user = response.data.User.username;
+      console.log($scope.user)
      }, function(response) {
           console.log(response);
         $scope.data = "Something went wrong";
